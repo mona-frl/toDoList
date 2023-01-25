ec7e45a4c91e48e2ef7226ecc7554c963d6b65b0
Monalisa <monihank@gmail.com>
2023-01-25 13:59:15 +0100

    V2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # [The Complete 2023 Web Development Bootcamp - Bootstrap Project](https://www.udemy.com/course/the-complete-web-development-bootcamp/)
 
-This is the to do list v1 using EJS templating.
+[V1] This is the to do list v1 using EJS templating.
+[V2] Now I have a database to store the items from the lists, also can delete the items from different lists and therefore, deleting from the database too.
--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//lodash
+const _ = require('lodash');
+
+//db 
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+
 //requires the local file of our created module
 const date = require(__dirname + '/date.js');
 
@@ -11,44 +19,130 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //array of items to the list of to do list 
-const items = [];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
+const itemSchema = {
+    name: String
+};
+
+
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+}
+
+const Item = mongoose.model("Item", itemSchema);
+const List = mongoose.model("List", listSchema);
+
+
+const defaultItem1 = new Item({ name: "Be happy 😊" });
+const defaultItem2 = new Item({ name: "Drink water 💧" });
+const defaultItem3 = new Item({ name: "Sleep 8h 💤" });
+
+const defaultItems = [defaultItem1, defaultItem2, defaultItem3]
+
+const day = date.getDate();
 
 //renders the list as the root file 
 app.get('/', (req, res) => {
-    //renders the ejs file
-    res.render('list', { listTitle: date.getDate(), newListItems: items });
+
+    Item.find({}, (err, foundItems) => {
+        if (err) {
+            console.error(err);
+        } else {
+            //looks for the foundItems lenght and adds the 3 default Items
+            if (foundItems.length === 0) {
+                Item.insertMany(defaultItems, (err) => {
+                    if (err) {
+                        console.log(err.message);
+                    } else {
+                        console.log("Items have been added to the DB.")
+                    }
+                });
+                res.redirect("/");
+            }
+            res.render('list', {
+                listTitle: day,
+                newListItems: foundItems
+            });
+        }
+    })
 })
 
-//renders a new list as the work list
-app.get('/work', (req, res) => {
-    res.render('list', { listTitle: "Work List", newListItems: workItems })
-})
+app.get("/:customListName", (req, res) => {
+    const customListName = _.capitalize(req.params.customListName);
 
-//shows about page
-app.get('/about', (req, res) => {
-    res.render('about')
-})
+    List.findOne({ name: customListName }, {}, (err, foundList) => {
+        if (!err) {
+            if (customListName === "About") {
+                res.render("about");
+                //checks if the foundlist exists & length is 0
+            } else if (!foundList || !foundList.length === 0) {
+                //Create a new list
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+                list.save(() =>
+                    res.redirect("/" + customListName));
+            } else {
+                //Show an existing list
+                res.render("list", {
+                    listTitle: foundList.name,
+                    newListItems: foundList.items
+                });
+            }
+        }
+    });
+});
 
 app.post('/', (req, res) => {
     // adds the item to the items array to display
-    let item = req.body.newItem;
+    const itemName = req.body.newItem;
+    const listName = req.body.list;
 
-    //checkes the list and display the right list
-    if (req.body.list === "work") {
-        workItems.push(item)
-        //posts the item to the page
-        res.redirect('/work')
-    } else {
-        items.push(item);
-        //posts the item to the page
+    //creates the item with the input
+    const item = new Item({
+        name: itemName
+    })
+
+    //saves the item to the right DB. the day one is the root one
+    if (listName === day) {
+        item.save();
         res.redirect('/')
+    }
+    else {
+        List.findOne({ name: listName }, (err, foundList) => {
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect('/' + listName);
+        })
     }
 })
 
+app.post('/delete', (req, res) => {
+    //creates the item to delete, by looking through the submite button using the value of the name "delete" that is the item's DB ID
+    const itemDelete = req.body.delete;
+    const listTitle = req.body.listTitle;
 
 
+    //checks if the list is the root list 
+    if (listTitle === day) {
+        Item.findByIdAndRemove(itemDelete, err => {
+            if (!err) {
+                console.log("Item has been successfully deleted.")
+                res.redirect("/");
+            }
+        })
+    } else {
+        List.findOneAndUpdate(
+            { name: listTitle }, { $pull: { items: { _id: itemDelete } } }, (err, foundList) => {
+                if (!err) {
+                    res.redirect('/' + listTitle)
+                }
+            })
+    }
+});
 
 
 app.listen(port, () => {
